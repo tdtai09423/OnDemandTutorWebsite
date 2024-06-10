4b70f46a1b89c300c68d1eb5b6548eee3a147b48
unknown <tranduytai7@gmail.com>
2024-06-10 17:15:19 +0700

    config connection string

--- BE/ODTDemoAPI/ODTDemoAPI/Entities/OnDemandTutorContext.cs
+++ BE/ODTDemoAPI/ODTDemoAPI/Entities/OnDemandTutorContext.cs
@@ -37,7 +37,7 @@ public partial class OnDemandTutorContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=Zincerious;Initial Catalog=OnDemandTutor;User ID=sa;Password=12345;Encrypt=False;Trust Server Certificate=True");
+        => optionsBuilder.UseSqlServer(new ConfigurationBuilder().AddJsonFile("appsettings.json").Build().GetConnectionString("OnDemandTutor"));
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
