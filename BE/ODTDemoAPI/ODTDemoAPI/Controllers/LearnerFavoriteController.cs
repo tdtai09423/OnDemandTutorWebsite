d313552d6394b6625a51def16c7c2fd35ccca31b
Hong An 1209 <duonghongan12092003@gmail.com>
2024-06-26 09:51:24 +0700

    learner-favorite

--- BE/ODTDemoAPI/ODTDemoAPI/Controllers/LearnerFavoriteController.cs
+++ BE/ODTDemoAPI/ODTDemoAPI/Controllers/LearnerFavoriteController.cs
@@ -1,51 +1,94 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using ODTDemoAPI.Services;
+﻿using Google;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using ODTDemoAPI.Entities;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace ODTDemoAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class LearnerFavouritesController : ControllerBase
+    public class LearnerFavouriteController : ControllerBase
     {
-        private readonly ILearnerFavouriteService _learnerFavouriteService;
+        private readonly OnDemandTutorContext _context;
 
-        public LearnerFavouritesController(ILearnerFavouriteService learnerFavouriteService)
+        public LearnerFavouriteController(OnDemandTutorContext context)
         {
-            _learnerFavouriteService = learnerFavouriteService;
+            _context = context;
         }
 
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<LearnerFavourite>>> GetLearnerFavourites()
+        // POST: api/LearnerFavourite
+        [HttpPost("add-favorite")]
+        public async Task<IActionResult> AddToFavourites(int learnerId, int tutorId)
         {
-            return await _learnerFavouriteService.GetAllLearnerFavouritesAsync();
+            var learner = await _context.Learners.FirstOrDefaultAsync(l => l.LearnerId == learnerId);
+            var tutor = await _context.Tutors.FirstOrDefaultAsync(t => t.TutorId == tutorId);
+
+            if (learner == null || tutor == null)
+            {
+                return NotFound(new { Message = "Learner or Tutor not found." });
+            }
+
+            var fav = await _context.LearnerFavourites.FirstOrDefaultAsync(f => f.TutorId == tutorId && f.LearnerId == learnerId);
+
+            if (fav != null)
+            {
+                return BadRequest("Existed favorite.");
+            }
+
+            var learnerFavourite = new LearnerFavourite
+            {
+                LearnerId = learnerId,
+                TutorId = tutorId,
+            };
+
+            _context.LearnerFavourites.Add(learnerFavourite);
+            await _context.SaveChangesAsync();
+
+            // Load the related entities to include them in the response
+            var favor = await _context.LearnerFavourites
+                         .Include(f => f.Learner)
+                         .Include(f => f.Tutor)
+                         .FirstOrDefaultAsync(f => f.LearnerId == learnerFavourite.LearnerId && f.TutorId == learnerFavourite.TutorId);
+
+            return Ok( new { learnerId = favor!.LearnerId, FavourInfo = favor! });
         }
 
-        [HttpGet("learner/{learnerId}")]
-        public async Task<ActionResult<IEnumerable<Tutor>>> GetLearnerFavouriteTutors(int learnerId)
+        // GET: api/LearnerFavourite
+        [HttpGet("get-all-favorite")]
+        public async Task<ActionResult<IEnumerable<LearnerFavourite>>> GetFavourites()
         {
-            var tutors = await _learnerFavouriteService.GetLearnerFavouriteTutorsAsync(learnerId);
-            if (tutors == null || tutors.Count == 0)
+            return await _context.LearnerFavourites.ToListAsync();
+        }
+
+        // GET: api/LearnerFavourite/{learnerId}
+        [HttpGet("{learnerId}")]
+        public async Task<ActionResult<IEnumerable<LearnerFavourite>>> GetFavouritesByLearnerId(int learnerId)
+        {
+            var favourites = await _context.LearnerFavourites
+                .Where(lf => lf.LearnerId == learnerId)
+                .ToListAsync();
+
+            return favourites;
+        }
+
+        // DELETE: api/LearnerFavourite/{learnerId}/{tutorId}
+        [HttpDelete("{learnerId}/{tutorId}")]
+        public async Task<IActionResult> RemoveFromFavourites(int learnerId, int tutorId)
+        {
+            var favourite = await _context.LearnerFavourites
+                .FirstOrDefaultAsync(lf => lf.LearnerId == learnerId && lf.TutorId == tutorId);
+
+            if (favourite == null)
             {
                 return NotFound();
             }
 
-            return tutors;
-        }
+            _context.LearnerFavourites.Remove(favourite);
+            await _context.SaveChangesAsync();
 
-        [HttpPost]
-        public async Task<ActionResult<LearnerFavourite>> AddLearnerFavourite(LearnerFavourite learnerFavourite)
-        {
-            await _learnerFavouriteService.AddLearnerFavouriteAsync(learnerFavourite);
-            return CreatedAtAction("GetLearnerFavouriteTutors", new { learnerId = learnerFavourite.LearnerId }, learnerFavourite);
-        }
-
-        [HttpDelete("{learnerId}/{tutorId}")]
-        public async Task<IActionResult> DeleteLearnerFavourite(int learnerId, int tutorId)
-        {
-            await _learnerFavouriteService.RemoveLearnerFavouriteAsync(learnerId, tutorId);
             return NoContent();
         }
     }
