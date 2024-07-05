4913832a9f57dd52bbd84c3d9fb193106f213c02
Zyncek <thangtqse181868@fpt.edu.vn>
2024-07-05 18:40:01 +0700

    Update chat entities (add JsonIgnore)

--- BE/ODTDemoAPI/ODTDemoAPI/Entities/ChatBox.cs
+++ BE/ODTDemoAPI/ODTDemoAPI/Entities/ChatBox.cs
@@ -1,4 +1,6 @@
-﻿namespace ODTDemoAPI.Entities
+﻿using System.Text.Json.Serialization;
+
+namespace ODTDemoAPI.Entities
 {
     public partial class ChatBox
     {
@@ -11,13 +13,13 @@
         public DateTime SendDate { get; set; }
 
         public int? LastMessageId { get; set; }
-
+        [JsonIgnore]
         public virtual ICollection<ChatMessage> ChatMessages { get; set; } = new List<ChatMessage>();
 
         public virtual ChatMessage? LastMessage { get; set; }
 
         public virtual Learner Learner { get; set; } = null!;
 
-        public virtual Learner Tutor { get; set; } = null!;
+        public virtual Tutor Tutor { get; set; } = null!;
     }
 }
