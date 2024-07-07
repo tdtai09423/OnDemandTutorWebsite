5528cd4096a3d1189660d2f583bd7d7940ce406c
hungfptu <hungnntse181824@fpt.edu.vn>
2024-07-07 19:42:37 +0700

    invoice and finish 2/3 UI of Notification

--- FE/on-demand-tutor-ui/src/Components/Layout/components/NotificationCom/index.js
+++ FE/on-demand-tutor-ui/src/Components/Layout/components/NotificationCom/index.js
@@ -2,18 +2,29 @@ import Card from 'react-bootstrap/Card';
 import { useEffect, useState } from 'react';
 import './notiCom.scss';
 const NotificationCom = ({ notificationInfo }) => {
-
+    const formatDate = (date) => {
+        const day = date.getDate();
+        const month = date.getMonth() + 1;
+        const year = date.getFullYear();
+        return `${year}-0${month}-${day}`;
+    };
+    console.log("NOTI in COM", notificationInfo)
 
     return (
         <>
             {notificationInfo.map((item, index) => {
+                const formattedOrderDate = formatDate(new Date(item.notificateDay));
+                const key = index;
                 return (
                     <Card className='containerCard'>
                         <Card.Body>
                             <Card.Text className='card-text'>
                                 {item.content}
                             </Card.Text>
-                            <Card.Subtitle>{item.notificationDay}</Card.Subtitle>
+                            <Card.Text className='card-text'>
+                                {formattedOrderDate}
+                            </Card.Text>
+
                         </Card.Body>
                     </Card>
                 )
