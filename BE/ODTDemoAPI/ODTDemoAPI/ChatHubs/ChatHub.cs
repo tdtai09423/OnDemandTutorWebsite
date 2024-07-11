3abb45a144589ff4124554a4adce2a21a95dcad0
Admin <tranduytai7@gmail.com>
2024-07-12 01:20:04 +0700

    Revert "Revert "Chat v1""
    This reverts commit abe3333d17b14c4d4593247f426be922101de200.

--- BE/ODTDemoAPI/ODTDemoAPI/ChatHubs/ChatHub.cs
+++ BE/ODTDemoAPI/ODTDemoAPI/ChatHubs/ChatHub.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using Google;
+using Microsoft.AspNetCore.SignalR;
 using ODTDemoAPI.Entities;
 using ODTDemoAPI.Services;
 using System.Collections.Concurrent;
@@ -8,45 +9,73 @@ namespace ODTDemoAPI.ChatHubs
 {
     public class ChatHub : Hub
     {
-        private readonly UserStatusService _userStatusService;
+        private readonly OnDemandTutorContext _context;
 
-        public ChatHub(UserStatusService userStatusService)
-        {
-            _userStatusService = userStatusService;
+        public ChatHub(OnDemandTutorContext context) {
+            _context = context;
         }
-        public async Task SendMessage(string sender, string message, int chatBoxId)
-        {
+
+        public async Task SendMessage(int chatBoxId, string sender, string message) {
+            var chatMessage = new ChatMessage {
+                ChatBoxId = chatBoxId,
+                Sender = sender,
+                Content = message,
+                SendDate = DateTime.UtcNow
+            };
+
+            _context.ChatMessages.Add(chatMessage);
+            await _context.SaveChangesAsync();
+
+            var chatBox = await _context.ChatBoxes.FindAsync(chatBoxId);
+            if (chatBox != null) {
+                chatBox.LastMessageId = chatMessage.Id;
+                chatBox.SendDate = chatMessage.SendDate;
+                _context.ChatBoxes.Update(chatBox);
+                await _context.SaveChangesAsync();
+            }
+
             await Clients.Group(chatBoxId.ToString()).SendAsync("ReceiveMessage", sender, message);
         }
 
-        public async Task JoinChatBox(int chatBoxId)
-        {
-            await Groups.AddToGroupAsync(Context.ConnectionId, chatBoxId.ToString());
+        public override async Task OnConnectedAsync() {
+            var chatBoxId = Context.GetHttpContext().Request.Query["chatBoxId"];
+            await Groups.AddToGroupAsync(Context.ConnectionId, chatBoxId);
+            await base.OnConnectedAsync();
         }
 
-        public async Task LeaveChatBox(int chatBoxId)
-        {
-            await Groups.RemoveFromGroupAsync(Context.ConnectionId, chatBoxId.ToString());
+        public override async Task OnDisconnectedAsync(Exception exception) {
+            var chatBoxId = Context.GetHttpContext().Request.Query["chatBoxId"];
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, chatBoxId);
+            await base.OnDisconnectedAsync(exception);
         }
+        //public async Task JoinChatBox(int chatBoxId)
+        //{
+        //    await Groups.AddToGroupAsync(Context.ConnectionId, chatBoxId.ToString());
+        //}
 
-        public override Task OnConnectedAsync()
-        {
-            var userId = GetUserIdFromContext();
-            _userStatusService.SetUserOnline(userId);
-            return base.OnConnectedAsync();
-        }
+        //public async Task LeaveChatBox(int chatBoxId)
+        //{
+        //    await Groups.RemoveFromGroupAsync(Context.ConnectionId, chatBoxId.ToString());
+        //}
 
-        public override Task OnDisconnectedAsync(Exception? exception)
-        {
-            var userId = GetUserIdFromContext();
-            _userStatusService.SetUserOffline(userId);
-            return base.OnDisconnectedAsync(exception);
-        }
+        //public override Task OnConnectedAsync()
+        //{
+        //    var userId = GetUserIdFromContext();
+        //    _userStatusService.SetUserOnline(userId);
+        //    return base.OnConnectedAsync();
+        //}
 
-        private int GetUserIdFromContext()
-        {
-            var userId = int.Parse(Context.User!.FindFirst("userId")!.Value);
-            return userId;
-        }
+        //public override Task OnDisconnectedAsync(Exception? exception)
+        //{
+        //    var userId = GetUserIdFromContext();
+        //    _userStatusService.SetUserOffline(userId);
+        //    return base.OnDisconnectedAsync(exception);
+        //}
+
+        //private int GetUserIdFromContext()
+        //{
+        //    var userId = int.Parse(Context.User!.FindFirst("userId")!.Value);
+        //    return userId;
+        //}
     }
 }
--- BE/ODTDemoAPI/ODTDemoAPI/Entities/ChatMessage.cs
+++ BE/ODTDemoAPI/ODTDemoAPI/Entities/ChatMessage.cs
@@ -11,6 +11,8 @@ namespace ODTDemoAPI.Entities
         public string Content { get; set; } = null!;
 
         public int ChatBoxId { get; set; }
+
+        public DateTime SendDate { get; set; }
         [JsonIgnore]
         public virtual ChatBox ChatBox { get; set; } = null!;
         [JsonIgnore]
